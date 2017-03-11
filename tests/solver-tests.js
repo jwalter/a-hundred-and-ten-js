fd797d44aab28ef09ed22d98fa975f8ff7e36d85
Joakim Waltersson <Joakim.waltersson@gmail.com>
2017-03-11 18:52:04 +0100

    Add test of two locations

--- tests/solver-tests.js
+++ tests/solver-tests.js
@@ -4,7 +4,11 @@ var Solver = require('../lib/solver')
 
 describe('Solver', function () {
     it('run() should return 1 when 1 is the only location', function() {
-        const locations = [1]
-        expect(Solver.run()).to.equal(1)
+        const locations = [[1]]
+        expect(Solver.run(locations)).to.eql([1])
+    }),
+    it('run() should return 1,2 when 1 has 2 as only neighbour', function() {
+        const locations = [[1, 2]]
+        expect(Solver.run(locations)).to.eql([1, 2])
     })
 })
