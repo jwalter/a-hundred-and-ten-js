4198cddcee9cdb3714b3dab9a2c89ce0111366f9
joakimwaltersson <joakim.waltersson@gmail.com>
2017-03-11 19:37:45 +0000

    Add Location class. Add test for path of length 3

--- tests/solver-tests.js
+++ tests/solver-tests.js
@@ -1,18 +1,32 @@
 var chai = require('chai')
 var expect = chai.expect
 var Solver = require('../lib/solver')
+var Location = require('../lib/location')
 
 describe('Solver', function () {
     it('run() should return 1 when 1 is the only location', function() {
-        const locations = [[1]]
-        expect(Solver.run(locations)).to.eql([1])
-    }),
+        const locations = [new Location(1, [])]
+        expect(Solver.findPath(locations)).to.eql([1])
+    })
     it('run() should return 1,2 when 1 has 2 as only neighbour', function() {
-        const locations = [[1, 2], [2, 1]]
-        expect(Solver.run(locations)).to.eql([1, 2])
+        const locations = [
+            new Location(1, [2]), 
+            new Location(2, [1])
+            ]
+        expect(Solver.findPath(locations)).to.eql([1, 2])
     })
+    
     it('run() should return 1 and 2 or 3 when 1 has 2 and 3 as neighbours but 2 and 3 are not neighbours', function() {
-        const locations = [[1, 2, 3], [2, 1], [3, 1]]
-        expect(Solver.run(locations)).to.have.lengthOf(2)
+        const locations = [
+            new Location(1, [2, 3]),
+            new Location(2, [1]),
+            new Location(3, [1])]
+        expect(Solver.findPath(locations)).to.have.lengthOf(2)
     })
+    
+    it('run() should return 1 and 2 and 3 when 1 is neighbour of 2 and 2 is neighbour of 3', function() {
+        const locations = [new Location(1, [2]), new Location(2, [1, 3]), new Location(3, [2])]
+        expect(Solver.findPath(locations)).is.eql([1, 2, 3])
+    })
+    
 })
