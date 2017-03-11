e80a3d54bb117e2b9f254ca5df4444dd9eadcc6c
Joakim Waltersson <Joakim.waltersson@gmail.com>
2017-03-11 18:16:23 +0100

    Add first test

--- tests/solver-tests.js
+++ tests/solver-tests.js
@@ -3,7 +3,8 @@ var expect = chai.expect
 var Solver = require('../lib/solver')
 
 describe('Solver', function () {
-    it('run() should return HelloWorld', function() {
-        expect(Solver.run()).to.be.a('number')
+    it('run() should return 1 when 1 is the only location', function() {
+        const locations = [1]
+        expect(Solver.run()).to.equal(1)
     })
 })
