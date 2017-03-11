6b22112908d4ab8721652de60e5eb251329ec4da
Joakim Waltersson <Joakim.waltersson@gmail.com>
2017-03-11 19:26:35 +0100

    Add test for finding path of length 2

--- tests/solver-tests.js
+++ tests/solver-tests.js
@@ -8,7 +8,11 @@ describe('Solver', function () {
         expect(Solver.run(locations)).to.eql([1])
     }),
     it('run() should return 1,2 when 1 has 2 as only neighbour', function() {
-        const locations = [[1, 2]]
+        const locations = [[1, 2], [2, 1]]
         expect(Solver.run(locations)).to.eql([1, 2])
+    })
+    it('run() should return 1 and 2 or 3 when 1 has 2 and 3 as neighbours but 2 and 3 are not neighbours', function() {
+        const locations = [[1, 2, 3], [2, 1], [3, 1]]
+        expect(Solver.run(locations)).to.have.lengthOf(2)
     })
 })
